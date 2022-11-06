745603f0ea03b411f75be33bea43ae24e01d4476
Paganini <paganini.xxi@gmail.com>
2022-11-06 01:59:39 -0300

    Initial state

---
+++ src/main/java/com/paganini/thurler/simpleTasker/SimpleTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.paganini.thurler.simpleTasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paganini/thurler/simpleTasker/SimpleTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paganini.thurler.simpleTasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
