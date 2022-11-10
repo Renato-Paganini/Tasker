b0ccae01c70a65e04f643d9fca1f7b0782be5638
Paganini <paganini.xxi@gmail.com>
2022-11-10 03:16:41 -0300

    "TaskService and small fixes"

---
+++ src/main/java/com/paganini/thurler/simpleTasker/services/TaskService.java
@@ -0,0 +1,8 @@
+package com.paganini.thurler.simpleTasker.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    
+}
--- src/main/java/com/paganini/thurler/simpleTasker/services/UserserService.java
+++ src/main/java/com/paganini/thurler/simpleTasker/services/UserserService.java
@@ -1,8 +1,14 @@
 package com.paganini.thurler.simpleTasker.services;
 
+import java.util.Optional;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.paganini.thurler.simpleTasker.models.User;
 import com.paganini.thurler.simpleTasker.repositories.TaskRepository;
 import com.paganini.thurler.simpleTasker.repositories.UserRepository;
 
@@ -13,9 +19,43 @@ public class UserserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private TaskRepository taskRepository;
 
-    
+    public User findByID(Integer id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+            "User not found: id:"+id+" type: " + User.class.getName()
+        ));
+    }
 
-    
+    //This is to save on the data base
+    //C< RUD
+    @Transactional
+    public User create(User obj){
+        //If someone sends an object with id, let's ignore it
+        obj.setId(null);
+        obj= this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+
+    }
+
+    //U
+    @Transactional
+    public User update(User obj){
+        User newObj = findByID(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+
+    public void delete(Integer id){
+        findByID(id);
+        try{
+            this.userRepository.deleteById(id);
+        }catch(Exception e){
+            throw new RuntimeException("User has relations");
+        }
+    }
 }
